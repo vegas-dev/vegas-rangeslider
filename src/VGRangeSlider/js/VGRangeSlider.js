529f2aac76384723b53db8cb52c94c1a4f50ecd3
Alexander Vetlugin <vetlugins@gmail.com>
2024-05-02 12:20:13 +0300

    up

--- src/VGRangeSlider/js/VGRangeSlider.js
+++ src/VGRangeSlider/js/VGRangeSlider.js
@@ -1,6 +1,25 @@
+const rangeSliderSettings = {
+	type: 'single' // or double
+}
+
 class VGRangeSlider {
 	constructor(element, arg ={}) {
-		console.log(arg)
+		this.isInit = false;
+
+		if (typeof element === 'string') {
+			element = document.querySelector(element);
+			if (!element) return false;
+		} else if (!element) {
+			return false;
+		}
+
+		this.init(element, arg);
+	}
+
+	init(element, arg) {
+		const _this = this;
+
+		_this.isInit = true;
 	}
 }
 
